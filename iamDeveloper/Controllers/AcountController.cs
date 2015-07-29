b1cd41e9c1444f962422a7956a90d345b079eb09
Mahdi.R <Mahdi.R.LN@GMail.com>
2015-07-30 00:47:27 +0430

    in the name of ALLAH

---
+++ iamDeveloper/Controllers/AcountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using iamDeveloper.DA;
+using iamDeveloper.Model;
+
+namespace iamDeveloper.Controllers
+{
+    public class AcountController : Controller
+    {
+        // GET: Acount
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User mUser)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    var users = new Users
+                    {
+                        ID = mUser.Id,
+                        FName = mUser.FName,
+                        LName = mUser.LName,
+                        E_Mail = mUser.EMail,
+                        Username = mUser.Username,
+                        Password = mUser.Password
+                    };
+
+                    ModelsContainer db = new ModelsContainer();
+                    db.Users.Add(users);
+                    db.SaveChanges();
+                    User u = new User();
+                    
+                    
+                    return RedirectToAction("OperationResult");
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("OperationResult");
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult OperationResult()
+        {
+            return View();
+        }
+    }
+}
